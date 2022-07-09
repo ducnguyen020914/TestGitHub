4ffba763d81b9f94cae84336512829921d2957f1
ducnguyen <75960032+itachi020914@users.noreply.github.com>
2022-07-09 16:12:36 +0700

    Update TestGitHub.java

--- TestGitHub/src/testgithub/TestGitHub.java
+++ TestGitHub/src/testgithub/TestGitHub.java
@@ -14,9 +14,12 @@ public class TestGitHub {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Duc vi dai");
+
+    public static void main(String[] args) { // TODO code application logic here
+        
+        
+        System.out.println("Hello World");
+
     }
     
 }
